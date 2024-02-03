429c518f0e83b837597353a8a1289eefc0f82c5b
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-03 22:41:58 +0100

    Make notes path based on user input

--- NotesApp.ps1
+++ NotesApp.ps1
@@ -36,10 +36,10 @@ while ($true) {
     # Searching notes for the provided keyword
     Search-Notes $Keyword
 
-    # Asking the user if they want to export the notes
+    # Give users the possibility to export notes
     $ExportChoice = Read-Host "Do you want to export the notes? (Y/N)"
     if ($ExportChoice -eq "Y") {
-        $ExportPath = "/Users/theaholtlund/Documents/Notes.txt"
+        $ExportPath = Read-Host "Enter the path for the exported file, such as C:\Notes\exported_notes.txt)"
         Export-Notes -Path $ExportPath
     }
 }
