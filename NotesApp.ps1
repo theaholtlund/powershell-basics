ab221e8ade6fdf5f7f8c560b607d320aeef00ff6
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-05 11:21:30 +0100

    Remove loop functionality, more user-friendly

--- NotesApp.ps1
+++ NotesApp.ps1
@@ -7,39 +7,39 @@
 # Creating a new instance of ArrayList, allowing users to add and remove elements dynamically
 $Script:Notes = New-Object System.Collections.ArrayList
 
-# Main loop to keep the program running until the user decides to exit
-while ($true) {
-    # Adding example notes to the array
-    Add-Note "This is the first note."
-    Add-Note "Learning PowerShell basics."
-    Add-Note "This is a cool and important note."
+# Adding example notes to the array
+Add-Note "This is the first note."
+Add-Note "Learning PowerShell basics."
+Add-Note "This is a cool and important note."
 
-    # Displaying all notes in the array
-    Show-Notes
+# Displaying all notes in the array
+Show-Notes
 
-    # Removing a note from the array based on user input
-    Remove-Note
+# Removing a note from the array based on user input
+Remove-Note
 
-    # Displaying notes after removing one
-    Show-Notes
+# Displaying notes after removing one
+Show-Notes
 
-    # Asking the user for the keyword to search
-    $Keyword = Read-Host "Enter the keyword to search for. Type 'exit' to quit."
-    if ($Keyword -eq "exit") {
-        break
-    }
-
-    # Searching notes for the provided keyword
-    Search-Notes $Keyword
-
-    # Give users the possibility to export notes
-    $ExportChoice = Read-Host "Do you want to export the notes? (Y/N)"
-    if ($ExportChoice -eq "Y") {
-        $ExportPath = Read-Host "Enter the path for the exported file:"
-        Export-Notes -Path $ExportPath
-    } elseif ($ExportChoice -eq "N") {
-        Write-Host "No export of notes at this time."
-    } else {
-        Write-Host "Invalid option. Please choose either 'Y' or 'N'."
-    }
+# Asking the user for the keyword to search
+$Keyword = Read-Host "Enter the keyword to search for. Type 'exit' to quit."
+if ($Keyword -eq "exit") {
+    exit
 }
+
+# Searching notes for the provided keyword
+Search-Notes $Keyword
+
+# Give users the possibility to export notes
+$ExportChoice = Read-Host "Do you want to export the notes? (Y/N)"
+if ($ExportChoice -eq "Y") {
+    $ExportPath = Read-Host "Enter the path for the exported file:"
+    Export-Notes -Path $ExportPath
+} elseif ($ExportChoice -eq "N") {
+    Write-Host "No export of notes at this time."
+} else {
+    Write-Host "Invalid option. Please choose either 'Y' or 'N'."
+}
+
+# Clear after export
+Clear-Notes 
