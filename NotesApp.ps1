e72ffec43d72d2ddf24f2d87230e0a53dff59e97
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-25 21:33:59 +0100

    Fix categories functionality

--- NotesApp.ps1
+++ NotesApp.ps1
@@ -1,14 +1,12 @@
 # This file is the main entry point for the PowerShell Notes Application
 # Responsible for handling user interactions and integrating imported functions
 
-# Importing functions and tests from other scripts
+# Importing functions from other scripts
 . ".\Categories.ps1"
 . ".\NotesFunctions.ps1"
 
-# Creating a new instance of ArrayList for notes
+# Creating ArrayLists for notes and categories
 $Script:Notes = New-Object System.Collections.ArrayList
-
-# Creating a new instance of ArrayList for categories, allowing for adding and removing elements dynamically
 $Script:Categories = New-Object System.Collections.ArrayList
 
 # Adding example categories
@@ -16,15 +14,15 @@ Add-Category "Work"
 Add-Category "Personal"
 
 # Adding example notes to the array
-Add-Note "This is the first note."
-Add-Note "Learning PowerShell basics."
-Add-Note "This is a cool and important note."
+Add-Note "This is the first note." "Work"
+Add-Note "Learning PowerShell basics." "Personal"
+Add-Note "This is a cool and important note." "Work"
 
 # Welcome message
 Write-Host "`nWelcome to PowerShell Notes Application!" -ForegroundColor Yellow
 
 # Menu options
-do {
+Do {
     Write-Host "`nChoose an option:"
     Write-Host "1. View Notes"
     Write-Host "2. Add a Note"
@@ -35,24 +33,24 @@ do {
     Write-Host "7. Assign Category to Note"
     Write-Host "8. Clear All Notes"
     Write-Host "9. Exit"
-    $choice = Read-Host "Enter your choice (1-9)"
+    $Choice = Read-Host "Enter your choice (1-9)"
 
-    switch ($choice) {
+    Switch ($Choice) {
         1 {
-            # Display all categories
+            # Display all categories and notes
             Show-Categories
-            # Display all notes
             Show-Notes
         }
         2 {
             # Add a note
-            $note = Read-Host "Enter your note"
-            Add-Note $note
+            $Note = Read-Host "Enter your note"
+            $Category = Read-Host "Enter the category for the note"
+            Add-Note $Note $Category
         }
         3 {
             # Edit a note
-            $index = Read-Host "Enter the index of the note you want to edit"
-            Edit-Note $index
+            $Index = Read-Host "Enter the index of the note you want to edit"
+            Edit-Note $Index
         }
         4 {
             # Remove a note
@@ -60,20 +58,19 @@ do {
         }
         5 {
             # Search notes
-            $keyword = Read-Host @"
+            $Keyword = Read-Host @"
 Enter keyword to search for.
 Please note that the search is case-sensitive and requires exact match.
-Type 'exit' to quit.
 "@
-            Search-Notes $keyword
+            Search-Notes $Keyword
         }
         6 {
             # Export notes
-            $exportChoice = Read-Host "Do you want to export the notes? (Y/N)"
-            if ($exportChoice -eq "Y") {
-                $exportPath = Read-Host "Enter the path for the exported file:"
-                Export-Notes -Path $exportPath
-            } elseif ($exportChoice -eq "N") {
+            $ExportChoice = Read-Host "Do you want to export the notes? (Y/N)"
+            if ($ExportChoice -eq "Y") {
+                $ExportPath = Read-Host "Enter the path for the exported file:"
+                Export-Notes -Path $ExportPath
+            } elseif ($ExportChoice -eq "N") {
                 Write-Output "No export of notes at this time."
             } else {
                 Write-Output "Invalid option. Please choose either 'Y' or 'N'."
@@ -81,9 +78,9 @@ Type 'exit' to quit.
         }
         7 {
             # Assign a category to a note
-            $indexToAssign = Read-Host "Enter the index of the note you want to assign to a category"
-            $categoryIndex = Read-Host "Enter the index of the category you want to assign the note to"
-            Assign-Category $indexToAssign $categoryIndex
+            $IndexToAssign = Read-Host "Enter the index of the note you want to assign to a category"
+            $CategoryIndex = Read-Host "Enter the index of the category you want to assign the note to"
+            Assign-Category $IndexToAssign $CategoryIndex
         }
         8 {
             # Clear all notes
@@ -92,10 +89,10 @@ Type 'exit' to quit.
         9 {
             # Exit
             Write-Host "Bye for now, see you again later!" -ForegroundColor Yellow
-            exit
+            Exit
         }
-        default {
+        Default {
             Write-Host "Invalid choice. Please enter a number from 1 to 9."
         }
     }
-} while ($true)
+} While ($true)
