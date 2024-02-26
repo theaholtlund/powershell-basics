0137fd1406a224f327807fa59e29c386b8cb171f
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-02-26 16:23:26 +0100

    Properly initialise categories

--- App/Categories.ps1
+++ App/Categories.ps1
@@ -1,16 +1,50 @@
 # This file contains functions for managing note categories in the PowerShell Notes Application
 
+# Function to initialize the $Script:Categories array
+Function Initialize-Categories {
+    if (-not $Script:Categories) {
+        $Script:Categories = New-Object System.Collections.ArrayList
+    }
+}
+
 # Function to add a new category
 Function Add-Category {
     param (
         [Parameter(Mandatory = $true)]
         [String]$Category
     )
+    Initialize-Categories
     $Script:Categories.Add($Category) | Out-Null
+}
+
+# Function to remove a category
+Function Remove-Category {
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]$Category
+    )
+    Initialize-Categories
+    $Script:Categories.Remove($Category) | Out-Null
+}
+
+# Function to rename a category
+Function Rename-Category {
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]$OldCategory,
+        [Parameter(Mandatory = $true)]
+        [String]$NewCategory
+    )
+    Initialize-Categories
+    $Index = $Script:Categories.IndexOf($OldCategory)
+    if ($Index -ge 0) {
+        $Script:Categories[$Index] = $NewCategory
+    }
 }
 
 # Function to display all categories
 Function Show-Categories {
+    Initialize-Categories
     Write-Output "Categories:"
     For ($i = 0; $i -lt $Script:Categories.Count; $i++) {
         Write-Output "$i - $($Script:Categories[$i])"
@@ -25,6 +59,8 @@ Function Set-Category {
         [Parameter(Mandatory = $true)]
         [int]$CategoryIndex
     )
+
+    Initialize-Categories
 
     if ($NoteIndex -ge 0 -and $NoteIndex -lt $Script:Notes.Count -and $CategoryIndex -ge 0 -and $CategoryIndex -lt $Script:Categories.Count) {
         $Script:NotesCategories[$NoteIndex] = $Script:Categories[$CategoryIndex]
