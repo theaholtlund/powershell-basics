263487aa62784fcdbb5aba926b03f5d28c847c12
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-06-24 13:17:27 +0200

    Query database table

--- SQLAzureConnect.ps1
+++ SQLAzureConnect.ps1
@@ -1,4 +1,5 @@
 # Load environment variables from environment file
+# Current setup requires Global Secure Access to be enabled
 $EnvFile = ".sqlconfig"
 Get-Content $EnvFile | ForEach-Object {
     If ($_ -match '^\s*([^#][^=]*)=(.*)$') {
@@ -16,3 +17,22 @@ $Password = $Env:SQL_PASSWORD
 
 # Build connection string
 $ConnectionString = "Server=$ServerName;Database=$DatabaseName;User ID=$Username;Password=$Password;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
+
+# Use the .NET SqlClient to query
+Add-Type -AssemblyName "System.Data"
+
+$Connection = New-Object System.Data.SqlClient.SqlConnection
+$Connection.ConnectionString = $ConnectionString
+$Connection.Open()
+
+$Command = $Connection.CreateCommand()
+$Command.CommandText = "SELECT TOP 10 * FROM sys.tables"
+
+$Reader = $Command.ExecuteReader()
+
+While ($Reader.Read()) {
+    Write-Output $Reader["Name"]
+}
+
+$Reader.Close()
+$Connection.Close()
